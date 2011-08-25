ae9bee80085c046efa3f93cb6ec98b329b5f6f57
Gavin Bunney <gavin.bunney@gmail.com>
2011-08-25 16:31:08 +1000

    Initial commit of bamboo rss parser

---
+++ ikara.py
@@ -0,0 +1,61 @@
+import urllib2
+import feedparser
+from time import mktime
+from datetime import datetime
+import time
+
+BAMBOO_BUILD_KEY = 'RACQICTPCC-BUILDTRUNK'
+#BAMBOO_BUILD_KEY = 'RACQICTPCC-DBSTATISTICS'
+BAMBOO_HOST      = '10.53.116.60'
+BAMBOO_PORT      = '8085'
+BAMBOO_USERNAME  = 'gbunney'
+BAMBOO_PASSWORD  = 'gbunney'
+BAMBOO_RSS_URL   = 'http://%s:%s/rss/createAllBuildsRssFeed.action?feedType=rssFailed&buildKey=%s' % (BAMBOO_HOST, BAMBOO_PORT, BAMBOO_BUILD_KEY)
+
+LATEST_BUILD      = dict()
+LATEST_BUILD_GUID = None
+PREV_LATEST_BUILD_GUID = None
+
+def retrieve_bamboo_feed():
+  auth = urllib2.HTTPBasicAuthHandler()
+  auth.add_password(BAMBOO_HOST, BAMBOO_HOST, BAMBOO_USERNAME, BAMBOO_PASSWORD)
+  global BUILD_FEED
+  BUILD_FEED = feedparser.parse(BAMBOO_RSS_URL, handlers=[auth])
+ 
+  global LATEST_BUILD
+  global LATEST_BUILD_GUID
+  global PREV_LATEST_BUILD_GUID
+
+#  PREV_LATEST_BUILD_GUID = LATEST_BUILD_GUID 
+
+  if len(BUILD_FEED.entries) > 0:
+    LATEST_BUILD = BUILD_FEED.entries[0]
+    LATEST_BUILD_GUID = LATEST_BUILD.guid
+
+def detect_failed_builds():
+  while True:
+    try:
+      retrieve_bamboo_feed()
+      if PREV_LATEST_BUILD_GUID != LATEST_BUILD_GUID:
+        print 'found new failed build!!'
+        print LATEST_BUILD.title
+
+      print 'waiting...'
+
+      description = LATEST_BUILD.description
+      lastIdx = 0
+      userSearchKey = '/browse/user/'
+      while description.find(userSearchKey, lastIdx) >= 0 and description.find(".</p>") > description.find(userSearchKey, lastIdx):
+        userIdx = description.find(userSearchKey, lastIdx)
+        userEndIdx = description.find(">", userIdx) - 1
+        print description[userIdx+len(userSearchKey):userEndIdx]
+        lastIdx = description.find(userSearchKey, lastIdx) + 1
+
+      #time.sleep(15)
+      break
+    except:
+      print 'bye!'
+      break
+
+retrieve_bamboo_feed()
+detect_failed_builds()
