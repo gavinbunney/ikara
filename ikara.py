c5259a22b7bcc1f18fbef757fbf10dca1d1fe8aa
Gavin Bunney <gavin.bunney@gmail.com>
2011-08-25 16:42:06 +1000

    Added notice

--- ikara.py
+++ ikara.py
@@ -1,7 +1,21 @@
+#!/usr/bin/python
+#
+# Copyright 2011 Gavin Bunney.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
 import urllib2
 import feedparser
-from time import mktime
-from datetime import datetime
 import time
 
 BAMBOO_BUILD_KEY = 'RACQICTPCC-BUILDTRUNK'
@@ -16,6 +30,9 @@ LATEST_BUILD      = dict()
 LATEST_BUILD_GUID = None
 PREV_LATEST_BUILD_GUID = None
 
+############################################################################
+# Retrieve the bamboo feed.
+############################################################################
 def retrieve_bamboo_feed():
   auth = urllib2.HTTPBasicAuthHandler()
   auth.add_password(BAMBOO_HOST, BAMBOO_HOST, BAMBOO_USERNAME, BAMBOO_PASSWORD)
@@ -32,6 +49,9 @@ def retrieve_bamboo_feed():
     LATEST_BUILD = BUILD_FEED.entries[0]
     LATEST_BUILD_GUID = LATEST_BUILD.guid
 
+############################################################################
+# Detect builds that have failed
+############################################################################
 def detect_failed_builds():
   while True:
     try:
@@ -48,7 +68,7 @@ def detect_failed_builds():
       while description.find(userSearchKey, lastIdx) >= 0 and description.find(".</p>") > description.find(userSearchKey, lastIdx):
         userIdx = description.find(userSearchKey, lastIdx)
         userEndIdx = description.find(">", userIdx) - 1
-        print description[userIdx+len(userSearchKey):userEndIdx]
+        print description[userIdx+len(userSearchKey) : userEndIdx]
         lastIdx = description.find(userSearchKey, lastIdx) + 1
 
       #time.sleep(15)
