178c72f563428ff8a7e8fd4eb401a34411002e82
Gavin Bunney <gavin.bunney@gmail.com>
2011-08-26 11:59:05 +1000

    Added list of culprits

--- ikara.py
+++ ikara.py
@@ -14,20 +14,20 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 #
+import sys
 import urllib2
 import feedparser
 import time
 
-BAMBOO_BUILD_KEY = 'RACQICTPCC-BUILDTRUNK'
-#BAMBOO_BUILD_KEY = 'RACQICTPCC-DBSTATISTICS'
 BAMBOO_HOST      = '10.53.116.60'
 BAMBOO_PORT      = '8085'
 BAMBOO_USERNAME  = 'gbunney'
 BAMBOO_PASSWORD  = 'gbunney'
-BAMBOO_RSS_URL   = 'http://%s:%s/rss/createAllBuildsRssFeed.action?feedType=rssFailed&buildKey=%s' % (BAMBOO_HOST, BAMBOO_PORT, BAMBOO_BUILD_KEY)
+BAMBOO_RSS_URL   = ''
 
 LATEST_BUILD      = dict()
 LATEST_BUILD_GUID = None
+LATEST_BUILD_USERS = list()
 PREV_LATEST_BUILD_GUID = None
 
 ############################################################################
@@ -43,7 +43,7 @@ def retrieve_bamboo_feed():
   global LATEST_BUILD_GUID
   global PREV_LATEST_BUILD_GUID
 
-#  PREV_LATEST_BUILD_GUID = LATEST_BUILD_GUID 
+  PREV_LATEST_BUILD_GUID = LATEST_BUILD_GUID 
 
   if len(BUILD_FEED.entries) > 0:
     LATEST_BUILD = BUILD_FEED.entries[0]
@@ -57,25 +57,41 @@ def detect_failed_builds():
     try:
       retrieve_bamboo_feed()
       if PREV_LATEST_BUILD_GUID != LATEST_BUILD_GUID:
-        print 'found new failed build!!'
+        print 'found new failed build!'
         print LATEST_BUILD.title
+      
+        description = LATEST_BUILD.description
+        global LATEST_BUILD_USERS
+        LATEST_BUILD_USERS = list()
+        lastIdx = 0
+        userSearchKey = '/browse/user/'
+        while description.find(userSearchKey, lastIdx) >= 0 and description.find(".</p>") > description.find(userSearchKey, lastIdx):
+          userIdx = description.find(userSearchKey, lastIdx)
+          userEndIdx = description.find(">", userIdx) - 1
+          LATEST_BUILD_USERS.append(description[userIdx+len(userSearchKey) : userEndIdx])
+          lastIdx = description.find(userSearchKey, lastIdx) + 1
+
+        for user in LATEST_BUILD_USERS:
+          print 'culprit: ' + user
 
       print 'waiting...'
+      time.sleep(2)
 
-      description = LATEST_BUILD.description
-      lastIdx = 0
-      userSearchKey = '/browse/user/'
-      while description.find(userSearchKey, lastIdx) >= 0 and description.find(".</p>") > description.find(userSearchKey, lastIdx):
-        userIdx = description.find(userSearchKey, lastIdx)
-        userEndIdx = description.find(">", userIdx) - 1
-        print description[userIdx+len(userSearchKey) : userEndIdx]
-        lastIdx = description.find(userSearchKey, lastIdx) + 1
-
-      #time.sleep(15)
-      break
     except:
       print 'bye!'
       break
 
-retrieve_bamboo_feed()
-detect_failed_builds()
+def main(args):
+  
+  if len(args) != 2:
+    print "Specify the Build Key to watch"
+    sys.exit(1)
+  
+  global BAMBOO_RSS_URL
+  BAMBOO_RSS_URL = 'http://%s:%s/rss/createAllBuildsRssFeed.action?feedType=rssFailed&buildKey=%s' % (BAMBOO_HOST, BAMBOO_PORT, args[1])
+
+  retrieve_bamboo_feed()
+  detect_failed_builds()
+  
+if __name__ == '__main__':
+  main(sys.argv)
