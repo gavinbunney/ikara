0f01c053db4ac18c8c59ceefdb4dd741a1ebc065
Gavin Bunney <gavin.bunney@gmail.com>
2011-08-26 13:54:41 +1000

    Merged with missile source from retaliation jenkins plugin

--- ikara.py
+++ ikara.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 #
-# Copyright 2011 Gavin Bunney.
+# Copyright 2011 4impact Technology Services, Brisbane, Australia http://www.4impact.com.au
+# 
+# Based on retaliation.py by PaperCut Software Int. Pty. Ltd. 
+# Copyright 2011 PaperCut Software Int. Pty. Ltd. http://www.papercut.com/
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -13,45 +16,203 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
+# 
+
+############################################################################
+# 
+# Ikara - Bamboo Blame!
 #
+############################################################################
+
 import sys
+import time
+import socket
+import urllib
+import re
+import json
+
+import usb.core
+import usb.util
+
 import urllib2
 import feedparser
-import time
+
+##########################  CONFIG   #########################
+
+#
+# Define a dictionary of "command sets" that map usernames to a sequence 
+# of commands to target the user (e.g their desk/workstation).  It's 
+# suggested that each set start and end with a "zero" command so it's
+# always parked in a known reference location. The timing on move commands
+# is milli-seconds. The number after "fire" denotes the number of rockets
+# to shoot.
+#
+COMMAND_SETS = {
+    "will" : (
+        ("zero", 0), # Zero/Park to know point (bottom-left)
+        ("right", 3250),
+        ("up", 540),
+        ("fire", 4), # Fire a full barrage of 4 missiles
+        ("zero", 0), # Park after use for next time
+    ),
+    "tom" : (
+        ("zero", 0), 
+        ("right", 4400),
+        ("up", 200),
+        ("fire", 4),
+        ("zero", 0),
+    ),
+    "chris" : (      # That's me - just dance around and missfire!
+        ("zero", 0),
+        ("right", 5200),
+        ("up", 500),
+        ("pause", 5000),
+        ("left", 2200),
+        ("down", 500),
+        ("fire", 1),
+    ),
+}
 
 BAMBOO_HOST      = '10.53.116.60'
 BAMBOO_PORT      = '8085'
 BAMBOO_USERNAME  = 'gbunney'
 BAMBOO_PASSWORD  = 'gbunney'
-BAMBOO_RSS_URL   = ''
+##########################  ENG CONFIG  #########################
+
+# The code...
+
+# Protocol command bytes
+DOWN    = 0x01
+UP      = 0x02
+LEFT    = 0x04
+RIGHT   = 0x08
+FIRE    = 0x10
+STOP    = 0x20
+
+DEVICE = None
 
 LATEST_BUILD      = dict()
 LATEST_BUILD_GUID = None
 LATEST_BUILD_USERS = list()
 PREV_LATEST_BUILD_GUID = None
 
-############################################################################
-# Retrieve the bamboo feed.
-############################################################################
+def usage():
+    print "Usage: ikara.py [command] [value]"
+    print ""
+    print "   commands:"
+    print "     monitor - poll the bamboo rss feed for new failed builds,"
+    print "               then unleash monitor!!"
+    print ""
+    print "     up    - move up <value> milliseconds"
+    print "     down  - move down <value> milliseconds"
+    print "     right - move right <value> milliseconds"
+    print "     left  - move left <value> milliseconds"
+    print "     fire  - fire <value> times (between 1-4)"
+    print "     zero  - park at zero position (bottom-left)"
+    print "     pause - pause <value> milliseconds"
+    print ""
+    print "     <command_set_name> - run/test a defined COMMAND_SET"
+    print "             e.g. run:"
+    print "                  ikara.py 'snappy'"
+    print "             to test targeting of snappy as defined in your command set."
+    print ""
+
+
+def setup_usb():
+    # Tested only with the Cheeky Dream Thunder
+    global DEVICE 
+    DEVICE = usb.core.find(idVendor=0x2123, idProduct=0x1010)
+
+    if DEVICE is None:
+        raise ValueError('Missile device not found')
+
+    DEVICE.set_configuration()
+
+
+def send_cmd(cmd):
+    DEVICE.ctrl_transfer(0x21, 0x09, 0, 0, [0x02, cmd, 0x00,0x00,0x00,0x00,0x00,0x00])
+
+
+def send_move(cmd, duration_ms):
+    send_cmd(cmd)
+    time.sleep(duration_ms / 1000.0)
+    send_cmd(STOP)
+
+
+def run_command(command, value):
+    command = command.lower()
+    if command == "right":
+        send_move(RIGHT, value)
+    elif command == "left":
+        send_move(LEFT, value)
+    elif command == "up":
+        send_move(UP, value)
+    elif command == "down":
+        send_move(DOWN, value)
+    elif command == "zero" or command == "park" or command == "reset":
+        # Move to bottom-left
+        send_move(DOWN, 2000)
+        send_move(LEFT, 8000)
+    elif command == "pause" or command == "sleep":
+        time.sleep(value / 1000.0)
+    elif command == "fire" or command == "shoot":
+        if value < 1 or value > 4:
+            value = 1
+        # Stabilize prior to the shot, then allow for reload time after.
+        time.sleep(0.5)
+        for i in range(value):
+            send_cmd(FIRE)
+            time.sleep(4.5)
+    else:
+        print "Error: Unknown command: '%s'" % command
+
+
+def run_command_set(commands):
+    for cmd, value in commands:
+        run_command(cmd, value)
+
 def retrieve_bamboo_feed():
   auth = urllib2.HTTPBasicAuthHandler()
   auth.add_password(BAMBOO_HOST, BAMBOO_HOST, BAMBOO_USERNAME, BAMBOO_PASSWORD)
   global BUILD_FEED
   BUILD_FEED = feedparser.parse(BAMBOO_RSS_URL, handlers=[auth])
- 
+  
   global LATEST_BUILD
   global LATEST_BUILD_GUID
   global PREV_LATEST_BUILD_GUID
-
+  
   PREV_LATEST_BUILD_GUID = LATEST_BUILD_GUID 
-
+  
   if len(BUILD_FEED.entries) > 0:
     LATEST_BUILD = BUILD_FEED.entries[0]
     LATEST_BUILD_GUID = LATEST_BUILD.guid
 
-############################################################################
-# Detect builds that have failed
-############################################################################
+def target_user(user):
+    match = False
+    for key in COMMAND_SETS:
+        if key.lower() == user.lower():
+            # We have a command set that targets our user so got for it!
+            run_command_set(COMMAND_SETS[key])
+            match = True
+            break
+    if not match:
+        print "WARNING: No target command set defined for user %s" % user
+
+
+def jenkins_get_responsible_user(job_name):
+    # Call back to Jenkins and determin who broke the build. (Hacky)
+    # We do this by crudly parsing the changes on the last failed build
+
+    changes_url = JENKINS_SERVER + "/job/" + job_name + "/lastFailedBuild/changes"
+    changedata = urllib.urlopen(changes_url).read()
+
+    # Look for the /user/[name] link
+    m = re.compile('"/user/([^/"]+)').search(changedata)
+    if m:
+        return m.group(1)
+    else:
+        return None
+                
 def detect_failed_builds():
   while True:
     try:
@@ -59,7 +220,7 @@ def detect_failed_builds():
       if PREV_LATEST_BUILD_GUID != LATEST_BUILD_GUID:
         print 'found new failed build!'
         print LATEST_BUILD.title
-      
+        
         description = LATEST_BUILD.description
         global LATEST_BUILD_USERS
         LATEST_BUILD_USERS = list()
@@ -70,28 +231,49 @@ def detect_failed_builds():
           userEndIdx = description.find(">", userIdx) - 1
           LATEST_BUILD_USERS.append(description[userIdx+len(userSearchKey) : userEndIdx])
           lastIdx = description.find(userSearchKey, lastIdx) + 1
-
+        
         for user in LATEST_BUILD_USERS:
-          print 'culprit: ' + user
-
+          target_user(user)
+      
       print 'waiting...'
-      time.sleep(2)
-
-    except:
+      time.sleep(5)
+    
+    except KeyboardInterrupt:
       print 'bye!'
       break
 
 def main(args):
-  
-  if len(args) != 2:
-    print "Specify the Build Key to watch"
-    sys.exit(1)
-  
-  global BAMBOO_RSS_URL
-  BAMBOO_RSS_URL = 'http://%s:%s/rss/createAllBuildsRssFeed.action?feedType=rssFailed&buildKey=%s' % (BAMBOO_HOST, BAMBOO_PORT, args[1])
 
-  retrieve_bamboo_feed()
-  detect_failed_builds()
-  
+    if len(args) < 2:
+        usage()
+        sys.exit(1)
+
+    setup_usb()
+
+    if args[1] == "monitor":
+      if len(args) != 3:
+        print "Specify the Build Key to watch"
+        sys.exit(1)
+      
+      print "Waiting for Bamboo failed builds..."
+      global BAMBOO_RSS_URL
+      BAMBOO_RSS_URL = 'http://%s:%s/rss/createAllBuildsRssFeed.action?feedType=rssFailed&buildKey=%s' % (BAMBOO_HOST, BAMBOO_PORT, args[2])
+      
+      detect_failed_builds()
+      # Will never return
+      return
+
+    # Process any passed commands or command_sets
+    command = args[1]
+    value = 0
+    if len(args) > 2:
+        value = int(args[2])
+
+    if command in COMMAND_SETS:
+        run_command_set(COMMAND_SETS[command])
+    else:
+        run_command(command, value)
+
+
 if __name__ == '__main__':
-  main(sys.argv)
+    main(sys.argv)
